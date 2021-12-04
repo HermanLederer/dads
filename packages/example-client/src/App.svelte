c3b98b7484b58273bc3572d009d57c19d907022a
Herman <germans.lederers@gmail.com>
2021-12-04 16:46:53 +0100

    Initialize example service client example with Svelte

---
+++ packages/example-client/src/App.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import Post from "./lib/Post.svelte";
+  import type { PostContent } from "./lib/Post.svelte";
+
+  const posts: PostContent[] = [
+    {
+      title: "Title",
+      text: "Lorem ipum",
+    },
+    {
+      title: "Title 2",
+      text: "Lorem ipum sdfgnjdfigu hseilruhg ioaweyghrt aehrguyaekj",
+    },
+    {
+      title: "Title",
+      text: "Lorem ipuse rfsdfzsdfm",
+    },
+    {
+      title: "Title",
+      text: "Lorem 23423 fdgsdfgsdfg sdfg sdgd",
+    },
+  ];
+</script>
+
+<header>
+  <h1>PinPon</h1>
+</header>
+<main>
+  {#each posts as post}
+    <Post content={post} />
+  {/each}
+</main>
---
+++ packages/example-client/src/lib/Post.svelte
@@ -0,0 +1,15 @@
+<script lang="ts" context="module">
+  export type PostContent = {
+    title: string;
+    text: string;
+  };
+</script>
+
+<script lang="ts">
+  export let content: PostContent;
+</script>
+
+<article>
+  <h3>{content.title}</h3>
+  <p>{content.text}</p>
+</article>
